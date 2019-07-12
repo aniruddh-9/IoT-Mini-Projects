3f2bef022efb74c3b2864acb87adc83f02e305c3
aniruddh-9 <aniruddh9.m@gmail.com>
2019-07-12 09:59:03 +0530

    delete

--- Control from VPS (cli)/device_status.py
+++
@@ -1,6 +0,0 @@
-from boltiot import Bolt
-api_key="dc29855b-44ed-4781-af5d-e9087ed4b597"
-device_id="BOLT1115914"
-mybolt = Bolt(api_key,device_id)
-response = mybolt.isOnline()
-print(response)
--- Control from VPS (cli)/led_brightness.py
+++
@@ -1,6 +0,0 @@
-from boltiot import Bolt
-api_key="dc29855b-44ed-4781-af5d-e9087ed4b597"
-device_id="BOLT1115914"
-mybolt = Bolt(api_key,device_id)
-response = mybolt.analogWrite('0','150')
-print(response)
