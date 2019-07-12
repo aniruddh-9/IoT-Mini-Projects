eb90b3ab09e34351cba83d8a57e658f6bae55355
aniruddh-9 <aniruddh9.m@gmail.com>
2019-07-12 09:54:23 +0530

    CLI files added

---
+++ Control from VPS (cli)/led_control.py
@@ -0,0 +1,6 @@
+from boltiot import Bolt
+api_key="dc29855b-44ed-4781-af5d-e9087ed4b597"
+device_id="BOLT1115914"
+mybolt = Bolt(api_key,device_id)
+response = mybolt.digitalWrite('0','HIGH')
+print(response)
